7f51d1d2a76df60702f4b97ef78a5f31a6b11025
Wart Claes <wart.claes@gmail.com>
2019-12-16 10:24:14 +0100

    fix(angular): remove createDist

--- build/angular.js
+++ build/angular.js
@@ -1,12 +1,9 @@
 const { spawnAsPromise } = require('./util');
 
-const createDist = require('./create-dist');
-
 module.exports = async (component) => {
 
     await Promise.all([
         spawnAsPromise('ng', 'lint', component),
         spawnAsPromise('ng', 'build', component),
-        createDist(component),
     ])
 }
